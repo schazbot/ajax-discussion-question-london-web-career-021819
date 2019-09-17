e3322c7ef78ea0ada9f8c7d1e6b16872e4925ee4
schazbot <charlotte.pearce1984@gmail.com>
2019-09-17 17:50:45 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,50 @@
+const container = document.querySelector('.container')
+const btn = document.querySelector('.btn')
+const image = document.getElementById('profile_picture')
+
 const fullname = document.getElementById("fullname");
-console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
+const street = document.getElementById("street")
+const city = document.getElementById("city")
+const state = document.getElementById("state")
+const postCode = document.getElementById("postcode")
+
+
+const phone = document.getElementById("phone")
+const cell = document.getElementById("cell")
+const email = document.getElementById("email")
+
+const dateOfBirth = document.getElementById("date_of_birth")
+
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+  btn.addEventListener('click', getRandomUser)
+
+
+  function getRandomUser() {
+    return fetch("https://randomuser.me/api/")
+      .then(resp => resp.json())
+      .then(data => {
+        const name = data.results[0].name
+
+        fullname.innerText = name.title + " " + name.first + " " + name.last
+
+        email.innerText = data.results[0].email
+        street.innerText = data.results[0].location.street
+        city.innerText = data.results[0].location.city
+        state.innerText = data.results[0].location.state
+
+        postCode.innerText = data.results[0].location.postcode
+        phone.innerText = data.results[0].phone
+        cell.innerText = data.results[0].cell
+        dateOfBirth.innerText = data.results[0].dob.date
+        image.src = data.results[0].picture.thumbnail
+
+      })
+  }
+
+
+
+
+  
 });
